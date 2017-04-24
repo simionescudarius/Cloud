eabeec89d4f9fa95cffd53b1c552b75fe5a23d5c
simionescudarius <simionescudarius23@gmail.com>
2017-04-24 19:45:19 +0300

    v1.9
    Map added
    Edit button added

--- Web/js/custom.js
+++ Web/js/custom.js
@@ -2,51 +2,71 @@ $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var isClear = true;
+        var onlyOnce = false;
+        var index = 0;
+        var noisePollution = document.getElementById('cmn-toggle-3');
+        var circleArray = [];
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: 37.090, lng: -95.712},
+          mapTypeId: 'terrain'
+        });
 
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: location,
-            zoom: 16,
-            panControl: false,
-            scrollwheel: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+         google.maps.event.addDomListener(noisePollution, 'click', function() {
+      if (Boolean(isClear) == true) {
+              isClear = false;
+              var citymap = {
+              chicago: {
+                center: {lat: 41.878, lng: -87.629},
+                population: 2714856
+              },
+              newyork: {
+                center: {lat: 40.714, lng: -74.005},
+                population: 8405837
+              },
+              losangeles: {
+                center: {lat: 34.052, lng: -118.243},
+                population: 3857799
+              },
+              vancouver: {
+                center: {lat: 49.25, lng: -123.1},
+                population: 603502
+              }
+            };
+
+             for (var city in citymap) {
+              var cityCircle = new google.maps.Circle({
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+              map: map,
+              center: citymap[city].center,
+              radius: Math.sqrt(citymap[city].population) * 100
+            });
+
+            circleArray[index] = cityCircle;
+            index++;
+          }
+        }else{
+          isClear = true;
+            for (var i = 0; i < index; i++){
+              var c = new google.maps.Circle();
+              console.log(c);
+              c = circleArray[i];
+              console.log(c);
+              c.setMap(null);
+            }
         }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
+    });
+  }
 
-        var markerImage = 'marker.png';
-
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon: markerImage
-        });
-
-        var contentString = '<div class="info-window">' +
-                '<h3>Info Window Content</h3>' +
-                '<div class="info-content">' +
-                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-                '</div>' +
-                '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-            maxWidth: 400
-        });
-
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-
-        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
-
-        map.set('styles', styles);
-
-
-    }
 
     google.maps.event.addDomListener(window, 'load', initMap);
 });
+
 
 $(document).ready(function(e){
     $('.search-panel .dropdown-menu').find('a').click(function(e) {
