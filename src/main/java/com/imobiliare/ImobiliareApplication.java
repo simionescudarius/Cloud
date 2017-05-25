7fa0e8d36707d2cf7d930b3b6e99989f4bef0350
simionescudarius <simionescudarius23@gmail.com>
2017-05-25 15:50:18 +0300

    v1.0 Empty application
    Project arhitecture created
    Class diagram updated
    Database script updated

---
+++ src/main/java/com/imobiliare/ImobiliareApplication.java
@@ -0,0 +1,15 @@
+package com.imobiliare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class ImobiliareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliareApplication.class, args);
+	}
+}
---
+++ src/main/java/com/imobiliare/models/User.java
@@ -0,0 +1,74 @@
+package com.imobiliare.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private long id;
+    
+    @NotNull
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @NotNull
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @NotNull
+    @Column(name = "email")
+    private String emal;
+    
+    @NotNull
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmal() {
+		return emal;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmal(String emal) {
+		this.emal = emal;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
